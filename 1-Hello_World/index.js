3903d1670c6a278ad103470f890a583878f10e5f
Sohail Shoukat <sohailshoukat131@gmail.com>
2025-01-11 23:31:41 +0500

    Hello World

---
+++ 1-Hello_World/index.js
@@ -0,0 +1,10 @@
+//console.log()
+//console.log() is used in debugging in the code;
+console.log("Hello World 🌎");
+
+// alert() 
+// alert() method displays an alert box over current window with popup message;
+alert("Hello , Every one  Welcome to Javascript.👋 ");
+
+// document.write()
+document.write(" <h1>Hello World ! <h1/>");
