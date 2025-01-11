c97cc8fb0cfdf629f260e118090b4ae5b6acfa3f
Sohail Shoukat <sohailshoukat131@gmail.com>
2025-01-12 00:23:45 +0500

    Square root of a number

--- 3-To find square root/index.js
+++ 3-To find square root/index.js
@@ -1,1 +1,8 @@
 // Project 03
+// The Math.sqrt() static method returns the square root of a number.
+let userNumber = parseInt(prompt("Please enter a number"));
+let findSquare = Math.sqrt(userNumber);
+alert(`The square root of  ${userNumber} is : ${findSquare}`);
+
+// Math.sqrt(0);  // Output: 0
+// Math.sqrt(-9); // Output: NaN
