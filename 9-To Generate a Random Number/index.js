4f4af9170009046593fa8a1dd90500a7c9259a47
Sohail Shoukat <sohailshoukat131@gmail.com>
2025-01-29 17:33:57 +0500

    To generate a random number

---
+++ 9-To Generate a Random Number/index.js
@@ -0,0 +1,15 @@
+// Project #09
+
+// Formulla '
+// Math.random () * (Highest number - lowest number) + lowest number
+
+let x = Math.random();
+console.log(x);
+x = x * (10 - 1) + 1;
+console.log(x);
+
+// Get a number integer number between a range
+let a = Math.random();
+console.log(a);
+a = a * (10 - 1) + 1;
+console.log(Math.floor(a));
